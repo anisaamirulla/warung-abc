8f88855c147226da596e2fcd43421cec5ba72f20
nadiazulfa <anisaamirula72@gmail.com>
2026-07-28 12:20:17 +0700

    blum selesai

---
+++ buat_awal.php
@@ -0,0 +1,17 @@
+<?php
+include 'config/koneksi.php';
+
+$nama = 'Administrator';
+$username = 'admin';
+$password = password_hash('admin123', PASSWORD_DEFAULT);
+$role = 'admin';
+
+$sql = "INSERT INTO tbl_user (nama_lengkap, username, password, role)";
+$sql = "VALUES ('$nama', '$username', '$password', '$role')";
+
+if (mysqli_query($koneksi, $sql)) {
+    echo 'user admin berhasil dibuat. silahkan hapus file ini.';
+} else {
+    echo 'gagal memuat user: '. mysqli_error($koneksi);
+}
+?>
